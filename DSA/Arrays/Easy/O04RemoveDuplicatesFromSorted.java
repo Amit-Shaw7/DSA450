e67c77e453194f8d79053ba67fc8ff7734e101ae
Amit-Shaw7 <maahiamit777@gmail.com>
2023-12-13 22:17:06 +0530

    Mereg sort added

---
+++ DSA/Arrays/Easy/O04RemoveDuplicatesFromSorted.java
@@ -0,0 +1,18 @@
+package DSA.Arrays.Easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class O04RemoveDuplicatesFromSorted {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 2, 3, 3, 4, 5, 6 };
+        int[] res = removeDuplicates(arr);
+        Arrays.toString(res);
+    }
+
+    public static int[] removeDuplicates(int[] arr) {
+        // System.out.println("Incompplete");
+        return new int[]{0};
+    }
+}
--- Sorting/Advanced/O01MergeSort.java
+++ Sorting/Advanced/O01MergeSort.java
@@ -1,7 +1,53 @@
 package Sorting.Advanced;
 
+import java.util.Arrays;
+
 public class O01MergeSort {
     public static void main(String[] args) {
-        // int
+        int[] arr = { 4, 3, 5, 6, 21, 1, 3, 4, 6 };
+        printArr(arr);
+        mergeSort(0, arr.length, arr);
+        printArr(arr);
+    }
+
+    public static void printArr(int[] arr) {
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void mergeSort(int st, int end, int[] arr) {
+        if (arr.length == 1 || st == end) {
+            return;
+        }
+        int mid = (st + end) / 2;
+
+        mergeSort(st, mid, arr);
+        mergeSort(mid + 1, end, arr);
+
+        mergeAlgo(st, mid + 1, end, arr);
+    }
+
+    public static void mergeAlgo(int low, int mid, int high, int[] arr) {
+        int i = 0;
+        int[] temp = new int[arr.length];
+        while (low <= mid && mid + 1 <= high) {
+            if (arr[low] > arr[mid + 1]) {
+                temp[i] = arr[mid + 1];
+                mid++;
+            } else {
+                temp[i] = arr[low];
+                low++;
+            }
+            i++;
+        }
+
+        while (low <= mid) {
+            temp[i] = arr[low];
+            i++;
+        }
+
+        while (mid <= high) {
+            temp[i] = arr[mid + 1];
+            i++;
+        }
     }
 }
