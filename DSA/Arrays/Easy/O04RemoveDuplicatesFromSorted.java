ca611732cb7a54939f744440ae01e13fd78630b0
Amit-Shaw7 <maahiamit777@gmail.com>
2023-12-14 20:49:51 +0530

    added remove duplicates solution

--- DSA/Arrays/Easy/O04RemoveDuplicatesFromSorted.java
+++ DSA/Arrays/Easy/O04RemoveDuplicatesFromSorted.java
@@ -1,18 +1,27 @@
 package DSA.Arrays.Easy;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+
+/*
+ * Return the size of the new array after removing all duplicates from it.
+ */
 
 public class O04RemoveDuplicatesFromSorted {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 2, 3, 3, 4, 5, 6 };
-        int[] res = removeDuplicates(arr);
-        Arrays.toString(res);
+        int res = removeDuplicates(arr);
+        System.out.println(res);
     }
 
-    public static int[] removeDuplicates(int[] arr) {
+    public static int removeDuplicates(int[] arr) {
         // System.out.println("Incompplete");
-        return new int[]{0};
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            set.add(arr[i]);
+        }
+
+        return set.size();
     }
 }
