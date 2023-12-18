b7a911b01fedf6ea29b56d361cba4368e08f91cd
Amit-Shaw7 <maahiamit777@gmail.com>
2023-12-18 20:49:24 +0530

    bubble sort added

--- Sorting/Advanced/O03BubbleSortRecursive.java
+++ Sorting/Advanced/O03BubbleSortRecursive.java
@@ -6,11 +6,29 @@ public class O03BubbleSortRecursive {
     public static void main(String[] args) {
         int[] arr = { 5, 4, 3, 2, 1 };
         System.out.println(Arrays.toString(arr));
-        bubbleSort(arr);
+        bubbleSort(arr, arr.length - 1, 0);
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void bubbleSort(int[] arr){
-        
+    public static void bubbleSort(int[] arr, int r, int c) {
+        // base case if r which starts from len(arr) reaches 0 -> return
+        if (r <= 0) {
+            return;
+        }
+
+        // If c == len(arr)-1      ->  one iteration complete starts from 0 again 
+        if (c == arr.length - 1) {
+            r--;
+            c = 0;
+        }
+
+        if (arr[c] > arr[c + 1]) {
+            // swap
+            int temp = arr[c];
+            arr[c] = arr[c + 1];
+            arr[c + 1] = temp;
+        }
+
+        bubbleSort(arr, r, c + 1);
     }
 }
