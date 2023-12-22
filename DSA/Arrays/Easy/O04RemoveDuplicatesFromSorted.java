113e2ef373a093891d9e47d4a8e2010f780742b3
Amit-Shaw7 <maahiamit777@gmail.com>
2023-12-22 14:03:08 +0530

    optimised removeDuplicates from array

--- DSA/Arrays/Easy/O04RemoveDuplicatesFromSorted.java
+++ DSA/Arrays/Easy/O04RemoveDuplicatesFromSorted.java
@@ -1,6 +1,5 @@
 package DSA.Arrays.Easy;
 
-import java.util.Arrays;
 import java.util.HashSet;
 
 /*
@@ -10,8 +9,8 @@ import java.util.HashSet;
 public class O04RemoveDuplicatesFromSorted {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 2, 3, 3, 4, 5, 6 };
-        int res = removeDuplicates(arr);
-        System.out.println(res);
+        System.out.println(removeDuplicates(arr));
+        System.out.println(removeDuplicatesOptimal(arr));
     }
 
     public static int removeDuplicates(int[] arr) {
@@ -23,5 +22,22 @@ public class O04RemoveDuplicatesFromSorted {
         }
 
         return set.size();
+    }
+
+    public static int removeDuplicatesOptimal(int[] arr) {
+        int i = 0;
+
+        if (arr.length == 1) {
+            return 1;
+        }
+
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[i] != arr[j]) {
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+
+        return i + 1;
     }
 }
