6d4b8404ad827a78369f4b5394ebdf3624bd6668
Amit-Shaw7 <maahiamit777@gmail.com>
2023-12-22 17:11:23 +0530

    optimal and better approach added in left rotate

--- DSA/Arrays/Easy/O06LeftRotateByK.java
+++ DSA/Arrays/Easy/O06LeftRotateByK.java
@@ -6,13 +6,28 @@ public class O06LeftRotateByK {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5 };
         int[] arr1 = { 1, 2, 3, 4, 5 };
-        int k = 3;
+        int[] arr2 = { 1, 2, 3, 4, 5 };
+        int k = 10;
+        System.out.println("---------------------Brute-----------------------");
+        System.out.print("Original : ");
         System.out.println(Arrays.toString(arr));
         leftRotateByK(arr, k);
+        System.out.print("After Rotation : ");
         System.out.println(Arrays.toString(arr));
-        System.out.println("--------------------------------------------");
-        leftRotateByKOptimal(arr1, k);
+
+        System.out.println("---------------------Better----------------------");
+        System.out.print("Original : ");
         System.out.println(Arrays.toString(arr1));
+        leftRotateByKBetter(arr1, k);
+        System.out.print("After Rotation : ");
+        System.out.println(Arrays.toString(arr1));
+
+        System.out.println("---------------------Optimal----------------------");
+        System.out.print("Original : ");
+        System.out.println(Arrays.toString(arr2));
+        leftRotateByKOptimal(arr2, k);
+        System.out.print("After Rotation : ");
+        System.out.println(Arrays.toString(arr2));
     }
 
     public static void leftRotateByK(int[] arr, int k) {
@@ -32,7 +47,7 @@ public class O06LeftRotateByK {
         arr[arr.length - 1] = temp;
     }
 
-    public static void leftRotateByKOptimal(int[] arr, int k) {
+    public static void leftRotateByKBetter(int[] arr, int k) {
         int n = arr.length;
 
         if (k > n) {
@@ -44,6 +59,26 @@ public class O06LeftRotateByK {
         for (int i = 0; i < k; i++) {
             System.out.println(i);
             leftRotateByOne(arr, k);
+        }
+    }
+
+    public static void leftRotateByKOptimal(int[] arr, int k) {
+        int n = arr.length;
+        k = k % n;
+
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+        reverse(arr, 0, n - 1);
+    }
+
+    public static void reverse(int[] arr, int st, int end) {
+        System.out.println("ran");
+        while (st < end) {
+            int temp = arr[st];
+            arr[st] = arr[end];
+            arr[end] = temp;
+            st++;
+            end--;
         }
     }
 }
