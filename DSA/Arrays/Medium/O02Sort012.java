cf10625367c5f423ab57a384bded8d390b91c18c
Amit-Shaw7 <maahiamit777@gmail.com>
2024-01-01 17:52:53 +0530

    added sort012 question in arrays

--- DSA/Arrays/Medium/O02Sort012.java
+++ DSA/Arrays/Medium/O02Sort012.java
@@ -6,11 +6,14 @@ public class O02Sort012 {
         int[] arr1 = { 1, 0, 2, 1, 2, 0 };
         int[] arr2 = { 1, 0, 2, 1, 2, 0 };
 
-        sortBrute(arr);
+        sortBrute(arr); // TC - O(n(logn)) || SC - O(N);
         System.out.println(Arrays.toString(arr));
 
-        sortBetter(arr1);
+        sortBetter(arr1); // TC - O(N) but iterating twice || SC - O(1);
         System.out.println(Arrays.toString(arr1));
+
+        sortOptimal(arr2); // TC - O(N) but iterating once || SC - O(1);
+        System.out.println(Arrays.toString(arr2));
     }
 
     public static void sortBrute(int[] arr) {
@@ -46,5 +49,31 @@ public class O02Sort012 {
             arr[idx] = 2;
             idx++;
         }
+    }
+
+    public static void sortOptimal(int[] arr) {
+        int n = arr.length;
+        int low = 0;
+        int mid = 0;
+        int high = n - 1;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[mid] == 0) {
+                swap(arr, mid, low);
+                mid++;
+                low++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            }else{
+                swap(arr , mid , high);
+                high--;
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int idx1, int idx2) {
+        int temp = arr[idx1];
+        arr[idx1] = arr[idx2];
+        arr[idx2] = temp;
     }
 }
